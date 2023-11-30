26029166fa9811f1e8d85f75c2d1eef1cf44df35
jiawei3311 <jiawei3311pro@gmail.com>
2023-11-30 21:47:59 +0800

    added REST API Endpoints and Unit Of Work pattern.

---
+++ CarRentalManagement/Server/Controllers/BookingsController.cs
@@ -0,0 +1,140 @@
+﻿using CarRentalManagement.Server.Data;
+using CarRentalManagement.Server.IRepository;
+using CarRentalManagement.Shared.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Policy;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        //private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
+
+        //public BookingsController(ApplicationDbContext context)
+        public BookingsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            //_context = context;
+        }
+
+        // GET: api/Bookings
+        [HttpGet]
+        //public async Task<ActionResult<IEnumerable<Booking>>> GetBookings()
+        public async Task<IActionResult> GetBookings()
+        {
+            //if (_context.Bookings == null)
+            //{
+            //    return NotFound();
+            //}
+            //return await _context.Bookings.ToListAsync();
+            var Bookings = await _unitOfWork.Bookings.GetAll();
+            return Ok(Bookings);
+        }
+
+        // GET: api/Bookings/5
+        [HttpGet("{id}")]
+        //public async Task<ActionResult<Booking>> GetBooking(int id)
+        public async Task<IActionResult> GetBooking(int id)
+        {
+            //if (_context.Bookings == null)
+            //{
+            //    return NotFound();
+            //}
+            //var Booking = await _context.Bookings.FindAsync(id);
+            var Booking = await _unitOfWork.Bookings.Get(q => q.Id == id);
+
+            if (Booking == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Booking);
+        }
+
+        // PUT: api/Bookings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBooking(int id, Booking Booking)
+        {
+            if (id != Booking.Id)
+            {
+                return BadRequest();
+            }
+
+            //_context.Entry(Booking).State = EntityState.Modified;
+            _unitOfWork.Bookings.Update(Booking);
+
+            try
+            {
+                //await _context.SaveChangesAsync();
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //if (!BookingExists(id))
+                if (!await BookingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Bookings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Booking>> PostBooking(Booking Booking)
+        {
+            //if (_context.Bookings == null)
+            //{
+            //    return Problem("Entity set 'ApplicationDbContext.Bookings'  is null.");
+            //}
+            //_context.Bookings.Add(Booking);
+            //await _context.SaveChangesAsync();
+            await _unitOfWork.Bookings.Insert(Booking);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetBooking", new { id = Booking.Id }, Booking);
+        }
+
+        // DELETE: api/Bookings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBooking(int id)
+        {
+            //if (_context.Bookings == null)
+            //{
+            //    return NotFound();
+            //}
+            //var Booking = await _context.Bookings.FindAsync(id);
+            var Booking = await _unitOfWork.Bookings.Get(q => q.Id == id);
+            //if (Booking == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //_context.Bookings.Remove(Booking);
+            //await _context.SaveChangesAsync();
+            await _unitOfWork.Bookings.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        //private bool BookingExists(int id)
+        private async Task<bool> BookingExists(int id)
+        {
+            //return (_context.Bookings?.Any(e => e.Id == id)).GetValueOrDefault();
+            var Booking = await _unitOfWork.Bookings.Get(q => q.Id == id);
+            return Booking != null;
+        }
+    }
+}
---
+++ CarRentalManagement/Server/Controllers/ColoursController.cs
@@ -0,0 +1,141 @@
+﻿using CarRentalManagement.Server.Data;
+using CarRentalManagement.Server.IRepository;
+using CarRentalManagement.Shared.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.Security.Policy;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColoursController : ControllerBase
+    {
+        //private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
+
+        //public ColoursController(ApplicationDbContext context)
+        public ColoursController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            //_context = context;
+        }
+
+        // GET: api/Colours
+        [HttpGet]
+        //public async Task<ActionResult<IEnumerable<Colour>>> GetColours()
+        public async Task<IActionResult> GetColours()
+        {
+            //if (_context.Colours == null)
+            //{
+            //    return NotFound();
+            //}
+            //return await _context.Colours.ToListAsync();
+            var Colours = await _unitOfWork.Colours.GetAll();
+            return Ok(Colours);
+        }
+
+        // GET: api/Colours/5
+        [HttpGet("{id}")]
+        //public async Task<ActionResult<Colour>> GetColour(int id)
+        public async Task<IActionResult> GetColour(int id)
+        {
+            //if (_context.Colours == null)
+            //{
+            //    return NotFound();
+            //}
+            //var Colour = await _context.Colours.FindAsync(id);
+            var Colour = await _unitOfWork.Colours.Get(q => q.Id == id);
+
+            if (Colour == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Colour);
+        }
+
+        // PUT: api/Colours/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutColour(int id, Colour Colour)
+        {
+            if (id != Colour.Id)
+            {
+                return BadRequest();
+            }
+
+            //_context.Entry(Colour).State = EntityState.Modified;
+            _unitOfWork.Colours.Update(Colour);
+
+            try
+            {
+                //await _context.SaveChangesAsync();
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //if (!ColourExists(id))
+                if (!await ColourExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Colours
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Colour>> PostColour(Colour Colour)
+        {
+            //if (_context.Colours == null)
+            //{
+            //    return Problem("Entity set 'ApplicationDbContext.Colours'  is null.");
+            //}
+            //_context.Colours.Add(Colour);
+            //await _context.SaveChangesAsync();
+            await _unitOfWork.Colours.Insert(Colour);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetColour", new { id = Colour.Id }, Colour);
+        }
+
+        // DELETE: api/Colours/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteColour(int id)
+        {
+            //if (_context.Colours == null)
+            //{
+            //    return NotFound();
+            //}
+            //var Colour = await _context.Colours.FindAsync(id);
+            var Colour = await _unitOfWork.Colours.Get(q => q.Id == id);
+            //if (Colour == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //_context.Colours.Remove(Colour);
+            //await _context.SaveChangesAsync();
+            await _unitOfWork.Colours.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        //private bool ColourExists(int id)
+        private async Task<bool> ColourExists(int id)
+        {
+            //return (_context.Colours?.Any(e => e.Id == id)).GetValueOrDefault();
+            var Colour = await _unitOfWork.Colours.Get(q => q.Id == id);
+            return Colour != null;
+        }
+    }
+}
